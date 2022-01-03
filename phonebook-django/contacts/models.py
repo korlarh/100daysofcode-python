f20b410eea8308f3a490a420d6d1a81758369735
korlarh <codesplasher.kola007@gmail.com>
2022-01-03 23:57:17 +0100

    working on models

--- phonebook-django/contacts/models.py
+++ phonebook-django/contacts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    title = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=255)
+    other_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=50)
+    job_title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    phone_work = models.CharField(max_length=14)
+    phone_mobile = models.CharField(max_length=14)
+    phone_home = models.CharField(max_length=14)
+    email_work = models.EmailField(max_length=255)
+    email_home = models.EmailField(max_length=255)
+    groups = models.CharField(max_length=255)
+    address = models.TextField()
+    relationship = models.CharField(max_length=255)
+    web_address = models.CharField(max_length=255)
+
+
